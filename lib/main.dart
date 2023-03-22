c735981dfe86bd343e24f9f57c6f8d59ebb6015c
Dode Teddy <dodeteddy@Teds-MacBook-Air.local>
2023-03-22 17:00:25 +0800

    api implementation

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,10 @@ import 'package:tugas_akhir_app/ui/pages/main_page/main_page.dart';
 import 'package:tugas_akhir_app/ui/shared/theme/theme.dart';
 import 'ui/pages/splash_screen/splash_screen.dart';
 import 'ui/pages/onboarding_page/onboarding_page.dart';
-import 'package:device_preview/device_preview.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 
 void main() {
   runApp(const MyApp());
-  // runApp(
-  //   DevicePreview(builder: (context) => const MyApp())
-  // );
 }
 
 class MyApp extends StatelessWidget {
@@ -22,8 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      // useInheritedMediaQuery: true,
-      // locale: DevicePreview.locale(context),
+
       builder: (context, child) => ResponsiveWrapper.builder(
         child,
         maxWidth: 1200,
@@ -42,7 +37,6 @@ class MyApp extends StatelessWidget {
         '/registration' :(context) => const RegistrationPage(),
         '/main' :(context) => const MainPage()
       },
-      // home: const MainPage()
     );
   }
 }
